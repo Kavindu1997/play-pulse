9a8f21d877658f100ef4431f4fd59937036db050
Kavindu1997 <kksamaraweera1997@gmail.com>
2024-02-06 15:29:37 +0530

    Implement searching

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,17 +1,30 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { FcSearch } from "react-icons/fc";
-import React from "react";
+import React, { useRef } from "react";
 
-const SearchInput = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const SearchInput = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
   return (
-    <InputGroup>
-      <InputLeftElement children={<FcSearch />} fontSize="1.8rem" />
-      <Input
-        borderRadius={20}
-        placeholder="Search games here..."
-        variant="filled"
-      />
-    </InputGroup>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (ref.current) onSearch(ref.current.value);
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<FcSearch />} fontSize="1.8rem" />
+        <Input
+          ref={ref}
+          borderRadius={20}
+          placeholder="Search games here..."
+          variant="filled"
+        />
+      </InputGroup>
+    </form>
   );
 };
 
