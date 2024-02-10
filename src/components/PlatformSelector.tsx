223a5f99fccbfaae503f2c8d5e88888fe2efe4ce
Kavindu1997 <kksamaraweera1997@gmail.com>
2024-02-10 16:49:08 +0530

    Fetch Platforms: React Query

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -19,7 +19,7 @@ const PlatformSelector = ({ onPlatformSelect, selectedPlatform }: Props) => {
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
+        {data?.results.map((platform) => (
           <MenuItem
             onClick={() => onPlatformSelect(platform)}
             key={platform.id}
